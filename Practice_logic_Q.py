cfab8f71430d7eb3e1dc5c7a95bd3a2d79172dbc
Munaam <munaam94@gmail.com>
2026-01-07 19:11:14 +0500

    practice q

--- Practice_logic_Q.py
+++ Practice_logic_Q.py
@@ -1810,40 +1810,55 @@
 #     print()
 
 
-# Q Leetcode issue
-# Valid Parentheses
-class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = [] 
-        for ch in s:
-            # Storing the opening brackets
-            if ch == "(" or ch == "{" or ch == "[":
-                stack.append(ch)
+# # Q Leetcode issue
+# # Valid Parentheses
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = [] 
+#         for ch in s:
+#             # Storing the opening brackets
+#             if ch == "(" or ch == "{" or ch == "[":
+#                 stack.append(ch)
 
-            else:
-                # if stack is empty
-                if not stack:
-                    return False
+#             else:
+#                 # if stack is empty
+#                 if not stack:
+#                     return False
                 
-                # pop the last element to compare
-                last = stack.pop()
+#                 # pop the last element to compare
+#                 last = stack.pop()
 
-                if ch == ")" and last != "(":
-                    return False
-                if ch == "}" and last != "{":
-                    return False
-                if ch == "]" and last != "[":
-                    return False
+#                 if ch == ")" and last != "(":
+#                     return False
+#                 if ch == "}" and last != "{":
+#                     return False
+#                 if ch == "]" and last != "[":
+#                     return False
                 
-        return len(stack) == 0
+#         return len(stack) == 0
 
-ob = Solution()
-print(ob.isValid("()[{[]()[][]}]"))
+# ob = Solution()
+# print(ob.isValid("()[{[]()[][]}]"))
 
 
 # print(arr)
 
 
+n = int(input("Enter number of rows: "))
+
+k = 0
+
+for i in range(n):
+    k = k + i
+
+m = k + n
+
+for i in range(n):
+    for j in range(i+1):
+        print(format(m, "<2" ), end = " ")
+        m -= 1
+    print()
+print(k, n, m)
 
 
 
