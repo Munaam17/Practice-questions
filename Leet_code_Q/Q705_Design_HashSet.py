4a9b2ae09ef04ef7c0cd9400367a31dd9a95e5e3
Munaam <munaam94@gmail.com>
2026-02-19 06:56:21 +0500

    Leetcode problem 'Design_HashSet'

---
+++ Leet_code_Q/Q705_Design_HashSet.py
@@ -0,0 +1,25 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.dictionary = {}
+
+    def add(self, key: int) -> None:
+        self.dictionary[key] = 1
+
+    def remove(self, key: int) -> None:
+        if key in self.dictionary:
+            del self.dictionary[key]   
+
+    def contains(self, key: int) -> bool:
+        return key in self.dictionary
+    
+Ob = MyHashSet()
+Ob.add(1)
+Ob.add(2)
+print(Ob.contains(1))
+print(Ob.contains(3))
+Ob.add(2)
+print(Ob.contains(2))
+Ob.remove(2)
+print(Ob.contains(2))   
+
